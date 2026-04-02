0610e0d541f0a065e9c302e4fab8c481525d1d06
AnuragK1318 <anuragkamble.work@gmail.com>
2026-04-02 18:19:31 +0530

    Counter App

---
+++ Counter/src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Counter');
+
+  count:number=0;
+
+  handleCounter(val:string)
+  {
+    if(val=='plus')
+    {
+      this.count=this.count+1;
+    }else if(val=='minus' && val>'0')
+    {
+      this.count=this.count-1;
+    }
+    else{
+      this.count=0;
+    }
+  }
+}
